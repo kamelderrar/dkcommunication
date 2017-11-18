3fd6dcd5787f7cee79b618302d36174ce9a8dedc
kamelderrar <contact@dkcommunication.fr>
2017-11-18 20:08:18 +0100

    Update config.php

--- src/public/includes/config.php
+++ src/public/includes/config.php
@@ -1,8 +1,8 @@
 <?php 
 define ("DB_HOST","localhost");
-define ("DB_LOGIN","dk");
-define ("DB_PASS","04071984");
-define ("DB_BDD","dkblog");
+define ("DB_LOGIN","");
+define ("DB_PASS","");
+define ("DB_BDD","");
 
 $bdd = mysql_connect(DB_HOST,DB_LOGIN,DB_PASS);
 mysql_select_db(DB_BDD);
